6d1ca3e1ffefa5a475ccd5ad3d31353165ea83f1
rafasiqalv <105326241+rafasiqalv@users.noreply.github.com>
2023-04-28 21:20:28 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 //Conexao com o banco de dados
-const sequelize = new Sequelize('postapp','root','root',{
-    host:"localhost",
+const sequelize = new Sequelize('sql10614853','sql10614853','mfcAXCWr4n',{
+    host:"sql10.freemysqlhosting.net",
     port:"3306",
     dialect:"mysql"
 });
